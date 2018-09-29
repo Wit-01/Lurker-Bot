8bde9319efb239cae169f5bba28e86ab1332d819
Wit-01 <43590243+Wit-01@users.noreply.github.com>
2018-09-29 12:35:55 -0700

    test2

--- lurkbot.py
+++ lurkbot.py
@@ -1,24 +1,24 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
+#from discord.ext.commands import Command_Prefix
 import asyncio
-
-
-
 client = discord.Client()
-bot = commands.Bot(command_prefix='-' )
+#bot = commands.Bot(command_prefix('.'))
 
-#Notifies when bot comes online
 @client.event
 async def on_ready():
-
-    print("Bot Online")
+    print("Bot Online") 
     await client.change_presence(game=discord.Game(name="In the Shadows..."))
-    await client.send_message(discord.Object(id='CHANNELIDHERE'), "**You feel an evil presence watching you...**")
+    #await client.send_message(discord.Object(id='CHANNELID'), "**You feel an evil presence watching you...**")
+
+#test command
+#@bot.command()
+#async def ping():
+    #await bot.say("what is it?")
 
 #Basic Hello World style message
 @client.event
-
 async def on_message(message):
 
     if message.author == client.user:
